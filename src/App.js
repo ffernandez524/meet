8f2acb4cfd358fe85530a6f517d116401c037967
ffernandez524 <flareniel@gmail.com>
2023-05-28 21:29:37 -0400

    4.3 Lamba Functions Setup
    Setup 3 AWS Lambda functions:
    getAuthURL
    getAccessToken
    getCalendarEvents
    Tested successfully!

--- src/App.js
+++ src/App.js
@@ -3,15 +3,45 @@ import './App.css';
 
 import CitySearch from './CitySearch';
 import EventList from './EventList';
-import NumberOfEvents from './numberOfEvents';
+import NumberOfEvents from './NumberOfEvents';
+import { extractLocations, getEvents } from './api';
 
 class App extends Component {
+  state = {
+    events: [],
+    locations: []
+  }
+
+  async componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if(this.mounted) {
+        this.setState({ events: events, locations: extractLocations(events) });
+      }      
+    });
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = (location === 'all') ?
+        events :
+        events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents
+      });
+    });
+  }  
+
   render() {
     return (
       <div className="App">
-        <CitySearch />
+        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+        <EventList events={this.state.events}/>
         <NumberOfEvents />
-        <EventList />
       </div>
     );
   }
--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react';
 
 class CitySearch extends Component {
-    state = {
-        query: '',
-        suggestions: []
-    }
+  state = {
+    query: '',
+    suggestions: [],
+    showSuggestions: undefined
+  }    
 
-    handleInputChanged = (event) => {
-        const value = event.target.value;
-        const suggestions = this.props.locations.filter((location) => {
-            return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-        });
-        this.setState({ query: value,
-        suggestions,
-     });
-    };
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    const suggestions = this.props.locations.filter((location) => {
+      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+    });
+    this.setState({ query: value,
+    suggestions,
+    });
+  };
 
-    handleItemClicked = (suggestion) => {
-        this.setState({
-            query: suggestion
-        });
-    };
+  handleItemClicked = (suggestion) => {
+    this.setState({
+      query: suggestion,
+      showSuggestions: false
+    });
 
-    render() {
+    this.props.updateEvents(suggestion);
+  };
+
+  render() {
     return (
       <div className='CitySearch'>
         <input
@@ -30,16 +34,19 @@ class CitySearch extends Component {
           className='city'
           value={this.state.query}
           onChange={this.handleInputChanged}
+          onFocus={() => { this.setState({ showSuggestions: true }) }}
         />
-        <ul className='suggestions'>
+        <ul className='suggestions' style={ this.state.showSuggestions ? {}: { display: 'none' }}>
             {this.state.suggestions.map((suggestion) => (
               <li 
               key={suggestion}
               onClick={() => this.handleItemClicked(suggestion)}
               >{suggestion}</li>
             ))}
-            <li key='all'>
-                <b>See all cities</b>
+            <li key='all'
+              onClick={() => this.handleItemClicked('all')}
+            >
+              <b>See all cities</b>
             </li>
         </ul>
       </div>
